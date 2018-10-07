d3abf291306b4fe0b38bee3d8581bc470838e61d
Syed Ahmed <sahmed@flyht.com>
2018-10-07 00:55:48 -0600

    1

---
+++ Helo2/src/test/java/com/flyht/alpha/Helo2/TestHelo2.java
@@ -0,0 +1,11 @@
+package com.flyht.alpha.Helo2;
+
+public class TestHelo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Helo2");
+	}
+
+}
